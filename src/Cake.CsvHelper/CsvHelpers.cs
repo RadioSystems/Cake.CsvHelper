df961309e1879d719a95262b42db5231d35a0b52
Adam Pluciński <adaskothebeast@gmail.com>
2021-12-05 13:08:59 +0100

    Upgraded do cake 2.0.0. Following cake-contrib guidelines

--- src/Cake.CsvHelper/CsvHelpers.cs
+++ src/Cake.CsvHelper/CsvHelpers.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using Cake.Core;
@@ -7,20 +8,23 @@ using Cake.Core.IO;
 using CsvHelper;
 using CsvHelper.Configuration;
 
-namespace Cake.CsvHelper {
+namespace Cake.CsvHelper
+{
     /// <summary>
     /// The CsvHelpers class for working with CSV Helper.
     /// </summary>
-    public sealed class CsvHelpers {
+    public sealed class CsvHelpers
+    {
         private readonly IFileSystem _fileSystem;
         private readonly ICakeEnvironment _environment;
 
         /// <summary>
-        /// Initializes new instance of the <see cref="CsvHelpers"/> class.
+        /// Initializes a new instance of the <see cref="CsvHelpers"/> class.
         /// </summary>
         /// <param name="fileSystem">The filesystem.</param>
         /// <param name="environment">The environment.</param>
-        public CsvHelpers(IFileSystem fileSystem, ICakeEnvironment environment) {
+        public CsvHelpers(IFileSystem fileSystem, ICakeEnvironment environment)
+        {
             _fileSystem = fileSystem;
             _environment = environment;
         }
@@ -33,19 +37,27 @@ namespace Cake.CsvHelper {
         /// <param name="classMap">The class map to use, null if not needed.</param>
         /// <param name="settings">The settings.</param>
         /// <returns>List of defined type.</returns>
-        public IEnumerable<T> ReadRecords<T>(FilePath csvFile, ClassMap classMap, CsvHelperSettings settings) {
-            if (csvFile == null) {
+        public IEnumerable<T> ReadRecords<T>(FilePath csvFile, ClassMap? classMap, CsvHelperSettings settings)
+        {
+            if (csvFile == null)
+            {
                 throw new ArgumentNullException(nameof(csvFile));
             }
-            if (settings == null) {
+
+            if (settings == null)
+            {
                 throw new ArgumentNullException(nameof(settings));
             }
+
             var file = GetFile(csvFile);
             using (var textReader = new StreamReader(file.OpenRead()))
-            using (var csvReader = new CsvReader(textReader)) {
-                if (classMap != null) {
-                    csvReader.Configuration.RegisterClassMap(classMap);
+            using (var csvReader = new CsvReader(textReader, CultureInfo.InvariantCulture))
+            {
+                if (classMap != null)
+                {
+                    csvReader.Context.RegisterClassMap(classMap);
                 }
+
                 return csvReader.GetRecords<T>().ToList();
             }
         }
@@ -58,29 +70,40 @@ namespace Cake.CsvHelper {
         /// <param name="records">The records to write.</param>
         /// <param name="classMap">The class map.</param>
         /// <param name="settings">The settings.</param>
-        public void WriteRecords<T>(FilePath csvFile, List<T> records, ClassMap classMap, CsvHelperSettings settings) {
-            if (csvFile == null) {
+        public void WriteRecords<T>(FilePath csvFile, IList<T> records, ClassMap classMap, CsvHelperSettings settings)
+        {
+            if (csvFile == null)
+            {
                 throw new ArgumentNullException(nameof(csvFile));
             }
-            if (records == null) {
+
+            if (records == null)
+            {
                 throw new ArgumentNullException(nameof(records));
             }
-            if (classMap == null) {
+
+            if (classMap == null)
+            {
                 throw new ArgumentNullException(nameof(classMap));
             }
-            if (settings == null) {
+
+            if (settings == null)
+            {
                 throw new ArgumentNullException(nameof(settings));
             }
 
             var file = GetFile(csvFile);
             using (var stream = file.OpenWrite())
             using (var textWriter = new StreamWriter(stream, settings.Encoding))
-            using (var csvWriter = new CsvWriter(textWriter)) {
-                csvWriter.Configuration.RegisterClassMap(classMap);
+            using (var csvWriter = new CsvWriter(textWriter, CultureInfo.InvariantCulture))
+            {
+                csvWriter.Context.RegisterClassMap(classMap);
                 csvWriter.WriteHeader<T>();
-                foreach (var record in records) {
+                foreach (var record in records)
+                {
                     csvWriter.WriteRecord(record);
                 }
+
                 textWriter.Close();
             }
         }
@@ -93,13 +116,19 @@ namespace Cake.CsvHelper {
         /// <param name="records">The records to write.</param>
         /// <param name="mapping">The property column mapping.</param>
         /// <param name="settings">The settings.</param>
-        public void WriteRecords<T>(FilePath csvFile, List<T> records, Dictionary<string, string> mapping,
-            CsvHelperSettings settings) {
-            if (mapping == null) {
+        public void WriteRecords<T>(
+            FilePath csvFile,
+            IList<T> records,
+            IDictionary<string, string> mapping,
+            CsvHelperSettings settings)
+        {
+            if (mapping == null)
+            {
                 throw new ArgumentNullException(nameof(mapping));
             }
+
             var customMap = new DefaultClassMap<T>();
-            customMap.AutoMap();
+            customMap.AutoMap(CultureInfo.InvariantCulture);
             WriteRecords(csvFile, records, customMap, settings);
         }
 
@@ -110,14 +139,20 @@ namespace Cake.CsvHelper {
         /// <param name="csvFile">The CSV file to write.</param>
         /// <param name="records">The records to write.</param>
         /// <param name="settings">The settings.</param>
-        public void WriteRecords<T>(FilePath csvFile, List<T> records, CsvHelperSettings settings) {
-            if (csvFile == null) {
+        public void WriteRecords<T>(FilePath csvFile, IList<T> records, CsvHelperSettings settings)
+        {
+            if (csvFile == null)
+            {
                 throw new ArgumentNullException(nameof(csvFile));
             }
-            if (records == null) {
+
+            if (records == null)
+            {
                 throw new ArgumentNullException(nameof(records));
             }
-            if (settings == null) {
+
+            if (settings == null)
+            {
                 throw new ArgumentNullException(nameof(settings));
             }
 
@@ -125,16 +160,20 @@ namespace Cake.CsvHelper {
             var file = GetFile(csvFile);
             using (var stream = file.OpenWrite())
             using (var textWriter = new StreamWriter(stream, settings.Encoding))
-            using (var csvWriter = new CsvWriter(textWriter)) {
+            using (var csvWriter = new CsvWriter(textWriter, CultureInfo.InvariantCulture))
+            {
                 csvWriter.WriteHeader<T>();
-                foreach (var record in records) {
+                foreach (var record in records)
+                {
                     csvWriter.WriteRecord(record);
                 }
+
                 textWriter.Close();
             }
         }
 
-        private IFile GetFile(FilePath path) {
+        private IFile GetFile(FilePath path)
+        {
             var file = path.IsRelative ? path.MakeAbsolute(_environment) : path;
             return _fileSystem.GetFile(file);
         }
